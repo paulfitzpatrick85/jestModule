39806b20eb24440705c5028cf5975c4b57be2a02
paulfitzpatrick85 <fitzer99@gmail.com>
2022-06-08 22:08:18 +0000

    forEach, testing html elements

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,7 +5,11 @@
  const buttonClick = require("../button");
 
  beforeEach(() => {
-     document.body.innerHTML = "<p id='par'></p>";
+     let fs = require("fs")        //fs = file system handling module used to open, read and write files
+     let fileContents = fs.readFileSync("index.html", "utf-8"); //use fs to read contents of html and store variable fileContents, open using utf-8 character set set
+     document.open();           
+     document.write(fileContents);
+     document.close();
  });
  
  describe("DOM tests", () => {
@@ -13,5 +17,8 @@
          buttonClick();
          expect(document.getElementById("par")
              .innerHTML).toEqual("You Clicked");
+     });
+     test("h1 should exist", () => {
+         expect(document.getElementsByTagName("h1").length).toBe(1); //if one h1 exists, length will be .toBe(1)
      });
  });
